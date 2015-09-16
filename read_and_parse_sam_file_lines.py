1e8b8acce83e89f5751a10f783792b96bed8483e
ThomasJonesSecurity <tjones@bu.edu>
2015-09-16 17:05:52 -0400

    Update read_and_parse_sam_file_lines.py
    Extra print statements added for testing

--- read_and_parse_sam_file_lines.py
+++ read_and_parse_sam_file_lines.py
@@ -1,5 +1,6 @@
 # Author: Thomas Jones
-# Last Modified: 9/11/2015
+# Last Modified: 9/14/2015
+# Written for python3 interpreter 
 #
 # MET CS 521 Information Structures in Python Project
 #-------------------------------------------------------------------
@@ -7,51 +8,47 @@
 # a dictionary attack against the hashed credentials.
 # NOTE: The development of a dictionary attack has not yet started.
 
-
 # Loading Modules
 import os.path
 
 # Initial Values For Testing
-sam_target_file = 'sam_test'  #sam test is a simulated file for testing
+sam_target_file = 'sam_test'  # sam test is a simulated file for testing
 
 def main():
-
-    #if the file identified by string sam_target_file is determined by os.path to be a valid file
+    # if the file string sam_target is determined valid by os.path
     if os.path.isfile(sam_target_file):
 
-        #open the file identified by string sam_target_file as sam_file
+        # open the file identified by string sam_target_file as sam_file
         sam_file = open(sam_target_file)
 
-        #read in sam_file line by line
+        # read in sam_file line by line
         for each_line in sam_file:
 
-            #start this line on a new line
-            print(each_line, end='')
+            # start this line on a new line
+            print("READ IN: " + each_line, end='')
 
-            #split the line at each delimiting semi-colon and grab username and hashes
+            # split line at each delimiting semicolon & assign
             sam_username = each_line.split(":")[0]
             sam_lm_hash = each_line.split(":")[2]
             sam_ntlm_hash = each_line.split(":")[3]
 
-            #output for testing
-            print(sam_username + " " + sam_lm_hash  + " " + sam_ntlm_hash)
+            # output for testing
+            print("PARSED:  " +sam_username + " " + sam_lm_hash + " " + sam_ntlm_hash)
 
-        #close file after all lines have been handled
+        # close file after all lines have been handled
         sam_file.close()
 
-    else: # os.path determined that the file identified by string sam_target_file did not exist
+    else:  # os.path determined that the file did not exist
         print("Error locating a file " + sam_target_file)
 
     return
 
-if __name__ == "__main__":  #prevents main from being blindly executed source is imported as module
+if __name__ == "__main__":  # stops main execution if imported as module
     main()
 
 
-
-#CITATIONS
-#
-# Learned to use a main function and protect against execution in moduels from:
+# CITATIONS
+# Learned to use a main function and protect against execution in modules from:
 # Guido van Rossum blog in 2003
 # https://www.artima.com/weblogs/viewpost.jsp?thread=4829
 #
