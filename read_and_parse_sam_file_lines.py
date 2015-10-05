32774778b4401289e11a4d4dfbdcae6b12837470
ThomasJonesSecurity <tjones@bu.edu>
2015-10-05 17:57:16 -0400

    Update read_and_parse_sam_file_lines.py
    Broken into functions.  Added LeakDB API lookup.

--- read_and_parse_sam_file_lines.py
+++ read_and_parse_sam_file_lines.py
@@ -10,37 +10,67 @@
 
 # Loading Modules
 import os.path
+import requests
 
 # Initial Values For Testing
-sam_target_file = 'sam_test'  # sam test is a simulated file for testing
+SAM_TARGET_FILE = 'sam_test'  # sam test is a simulated file for testing
 
-def main():
+def read_and_parse_sam_file_lines(sam_filename):
+    accounts = {}
+
     # if the file string sam_target is determined valid by os.path
-    if os.path.isfile(sam_target_file):
+    if os.path.isfile(sam_filename):
 
-        # open the file identified by string sam_target_file as sam_file
-        sam_file = open(sam_target_file)
+        # open the file identified by string SAM_TARGET_FILE as sam_file
+        sam_file = open(sam_filename)
 
         # read in sam_file line by line
         for each_line in sam_file:
-
+            users_hashes = []
             # start this line on a new line
-            print("READ IN: " + each_line, end='')
+            #print("READ IN: " + each_line, end='')
 
             # split line at each delimiting semicolon & assign
             sam_username = each_line.split(":")[0]
             sam_lm_hash = each_line.split(":")[2]
             sam_ntlm_hash = each_line.split(":")[3]
 
+            users_hashes.append(sam_ntlm_hash)
+            users_hashes.append(sam_lm_hash)
+            accounts.update({sam_username:users_hashes})
+
             # output for testing
-            print("PARSED:  " +sam_username + " " + sam_lm_hash + " " + sam_ntlm_hash)
+            #print(accounts)
 
         # close file after all lines have been handled
         sam_file.close()
 
     else:  # os.path determined that the file did not exist
-        print("Error locating a file " + sam_target_file)
+        print("Error locating a file " + sam_filename)
 
+    return accounts
+
+
+def online_hash_lookup_by_leakedb_api(dictionary_accounts):
+    # adopted from http://blog.abusix.com/2013/07/08/using-leakdb-for-compromised-password-hashes/
+    # Requests dependency installed by $ sudo pip install requests
+    #  resolve given hashes to plain text passwords: leakdb-hashes.py
+
+    for key, value in dictionary_accounts.items():
+        for hash_value in value:
+            print(hash_value)
+            #Use the j parameter for json output
+            r = requests.get('https://api.leakdb.net/?j=%s' % hash_value)
+            json = r.json()
+            if json['found'] == 'true':
+                print('plaintext for %s => %s' % (hash_value, json['hashes'][0]['plaintext']))
+            else:
+               print('plaintext for %s not found ' % (hash_value))
+
+
+def main():
+    credentials = read_and_parse_sam_file_lines(SAM_TARGET_FILE)
+    online_hash_lookup_by_leakedb_api(credentials)
     return
 
 if __name__ == "__main__":  # stops main execution if imported as module
