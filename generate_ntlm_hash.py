99f9aef89a3c0a4c297d8dbf5ca796a62011d5aa
ThomasJonesSecurity <tjones@bu.edu>
2015-10-20 20:51:43 -0400

    Delete generate_ntlm_hash.py

--- generate_ntlm_hash.py
+++
@@ -1,5 +0,0 @@
-# adopted from https://www.trustedsec.com/march/generate-an-ntlm-hash-in-3-lines-of-python/
-
-import hashlib,binascii
-hash = hashlib.new('md4', "password".encode('utf-16le')).digest()
-print(binascii.hexlify(hash))
