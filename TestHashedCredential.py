9018d157ca1dc79e7581e19fd76bfc102935998a
ThomasJonesSecurity <tjones@bu.edu>
2015-10-21 08:48:08 -0400

    Update TestHashedCredential.py

--- TestHashedCredential.py
+++ TestHashedCredential.py
@@ -1,15 +1,34 @@
+import unittest
 import HashedCredential
 
-record = HashedCredential.HashedCredential(username = 'admin', ntlm_hash = '8846F7EAEE8FB117AD06BDD830B7586C', lm_hash = 'E52CAC67419A9A224A3B108F3FA6CB6D')
+class TestHashedCredentials(unittest.TestCase):
 
-record.write_output()
-print("\nLogical is_cracked(): False <----> ", record.is_cracked())
-print("\nStatus: hashes validated <----> ", record.status)
-record.cracked("password")
+    def test_HashedCredential_init_(self):
+        test_good_record = HashedCredential.HashedCredential(username = 'admin', ntlm_hash = '8846F7EAEE8FB117AD06BDD830B7586C', lm_hash = 'E52CAC67419A9A224A3B108F3FA6CB6D')
+        self.assertTrue(test_good_record.valid_hashes)
+        self.assertEqual(test_good_record.status, 'hashes validated')
 
-record.write_output()
-print("\nLogical is cracked: False <----> ", record.is_cracked())
-print("\nStatus: hashes validated <----> ", record.status)
+        test_bad_hash_record = HashedCredential.HashedCredential(username = 'admin', ntlm_hash = 'TESTBAdData8846F7EAEE8FB117AD06BDD830B7586C', lm_hash = 'E52CAC67419A9A224A3B108F3FA6CB6D')
+        self.assertFalse(test_bad_hash_record.valid_hashes)
+        self.assertEqual(test_bad_hash_record.status, 'NTLM hash length or character invalid')
 
-record.update_status('freely defined status')
-print("\nStatus: freely defined status <----> ", record.status)
+    def test_HashedCredential_cracked(self):
+        test_crack_record = HashedCredential.HashedCredential(username = 'admin', ntlm_hash = '8846F7EAEE8FB117AD06BDD830B7586C', lm_hash = 'E52CAC67419A9A224A3B108F3FA6CB6D')
+        self.assertFalse(test_crack_record.cracked_yet)
+
+        test_crack_record.cracked("password")
+        self.assertTrue(test_crack_record.cracked_yet)
+        self.assertEqual(test_crack_record.status, 'successfully cracked')
+        self.assertEqual(test_crack_record.plaintext, 'password')
+
+    def test_HashedCredential_update_status(self):
+        test_good_record = HashedCredential.HashedCredential(username = 'admin', ntlm_hash = '8846F7EAEE8FB117AD06BDD830B7586C', lm_hash = 'E52CAC67419A9A224A3B108F3FA6CB6D')
+        self.assertEqual(test_good_record.status, 'hashes validated')
+
+        test_good_record.update_status('free text for testing')
+        self.assertEqual(test_good_record.status, 'free text for testing')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
