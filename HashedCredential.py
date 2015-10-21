1b9d8c17766cb1f4793e6c7359b8c17d69b5b88b
ThomasJonesSecurity <tjones@bu.edu>
2015-10-21 14:40:31 -0400

    Update HashedCredential.py

--- HashedCredential.py
+++ HashedCredential.py
@@ -1,4 +1,4 @@
-import string
+import string # used to confrim hex characters
 
 # Windows SAM Files store usernames, lm and ntlm hashes.
 # This class, HashedCredential, intends to handle the credentials and
@@ -7,21 +7,35 @@ import string
 class HashedCredential:
 
     def __init__(self, username, lm_hash, ntlm_hash):
+    # Intent: Object stores username, ntlm hash string, and lm has string
+    #         Also provide status meta-data indicated status of cracking attempt
+    # Precondition: all arguments provided as single-line strings
+    # Postcondition: initilizes a object containing the strings from arguments
+    #         Post 1:   as well as default (un-cracked) status values
+    #         Post 2:   does some basic length and character checks to help
+    #                   validate the input hash strings
 
+        #Post 1 - default values
         self.cracked_yet = False
         self.plaintext = ''
         self.username = username
 
-        self.valid_hashes = False # invalid until proven otherwise
+        # invalid until proven otherwise
+        self.valid_hashes = False
         self.status = 'LM and NTLM hashes invalid'
         self.lm = 'Invalid LM hash'
         self.ntlm = 'Invalid NTLM hash'
 
+
+        # Post 1 - hexidecimal and length check of lm_hash and ntlm_hash
+
         def is_hex(test_for_hex):
+        # used to confrim hex characters
             return all(c in string.hexdigits for c in test_for_hex)
 
-        # Checking each hash lengths == 32 and only hex chars
         if len(lm_hash) == 32 and is_hex(lm_hash):
+        # lm_hash and ntlm_hash strings must contain hexidecimal characters only
+        # Checking each hash lengths == 32 and only hex chars
             self.lm = lm_hash
             self.status = 'NTLM hash length or character invalid'
             if len(ntlm_hash) == 32 and is_hex(ntlm_hash):
@@ -29,18 +43,29 @@ class HashedCredential:
                 self.valid_hashes = True
                 self.status = 'hashes validated'
 
+        if self.valid_hashes == False:
+            print("WARNING: Likely Bad Hash Input : ", self.status)
+
     def cracked(self, new_plaintext):
+    # Intent: update object with provided plaintext password
+    # Precondition: assuming that the function provides a plaintext password string
+    #               that is the proper cracked password from the hashes contained in the object
+    # Postcondition: updates status, cracked_yet flag, and plaintext
+    #                   validate the input hash strings
         self.cracked_yet = True
         self.plaintext = new_plaintext
         self.status = 'successfully cracked'
 
-    def is_cracked(self):
-        return self.cracked_yet
-
     def update_status(self,new_status):
+    # Intent: self.status is using a free-text string to describe the object's state
+    # Precondition: a text string is provided as the new_status
+    # Postcondition: updates status
          self.status = new_status
 
     def write_output(self):
+    # Intent: Provides basic format to ouptut all object values with descriptions
+    # Precondition: Object contains printable values
+    # Postcondition: prints output of all object values to screen
           print("\n\n Username: {0.username} \n Status: {0.status} \n Cracked: {0.cracked_yet} \n Plaintext: {0.plaintext} \
                  \n LM Hash: {0.lm} \n NTLM Hash: {0.ntlm} \n Valid Hashes: {0.valid_hashes}".format(self))
 
